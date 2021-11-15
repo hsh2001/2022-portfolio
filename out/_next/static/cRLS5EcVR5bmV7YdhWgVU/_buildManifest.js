bbc76a3f0a49705524433b9e118492cf408d0b66
os5633 <71377244+os5633@users.noreply.github.com>
2021-11-15 12:05:08 +0900

    Deploy Next.js to gh-pages

---
+++ out/_next/static/cRLS5EcVR5bmV7YdhWgVU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/90-51774305637769a74133.js","static/chunks/pages/index-f2dea33e59d0c80aeb63.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
